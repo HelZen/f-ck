cce5d6eeb1a78fe8853e1231b7cf1d6ee072dbe4
FERGUDOM000 <FERGUDOM000@BGSD.k12.wa.us>
2020-01-22 09:42:50 -0800

    ass

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -5,15 +5,58 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
+    public Rigidbody2D rb;
+
+    public float moveSpeed;
+    public float sprintSpeed;
+
+    public float baseHealth;
+    public float baseShield;
+    public float baseStamina;
+    public float baseMana;
+
+    public float baseMeleeDamage;
+    public float baseMeleeCooldown;
     
+    public float baseRangedDamage;
+    public float baseRangedCooldown;
+
+    public float baseMagicPower;
+    public float baseMagicCost;
+    public float baseMagicCooldown;
+
+    public string[] meleeWeaponsList;
+    public string[] rangedWeaponsList;
+
+    private void Awake()
+    {
+    
+        //rangedWeaponsList = {
+        //    "Bow",
+        //    -1
+        //};
+
+    }
+
     void Start()
     {
-        
+    
+
+    
     }
 
     void Update()
     {
+
+
+
+    }
+
+    void FixedUpdate()
+    {
+    
         
+
     }
 
 }
