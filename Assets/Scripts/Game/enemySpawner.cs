a3a4f3207480ccf3482e65158b847604bdaee733
FERGUDOM000 <FERGUDOM000@BGSD.k12.wa.us>
2020-01-24 08:43:04 -0800

    added broken enemy spawner

---
+++ Assets/Scripts/Game/enemySpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemySpawner : MonoBehaviour
+{
+    public GameObject enemy;
+
+    public float spawnTime;
+    public float spawnAmount;
+    float timeInSeconds;
+
+    void Update()
+    {
+        timeInSeconds += Time.deltaTime;
+        if (timeInSeconds % spawnTime == 0)
+        {
+            for (int i = 0; i < spawnAmount; i++)
+            {
+                Vector2 randomPositionOnScreen = Camera.main.ViewportToWorldPoint(new Vector2(Random.value, Random.value));
+                Instantiate(enemy, randomPositionOnScreen, Quaternion.identity);
+            }
+        }
+        
+    }
+
+}
